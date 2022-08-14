3a9cd05146050aa45ca8c8a5229b7879d9466c9f
akasata nana <akasantanana0894@gmail.com>
2022-08-15 01:29:41 +0900

    Initial Commit

---
+++ KaraKan/KaraKanApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaraKanApp.swift
+//  KaraKan
+//
+//  Created by Akasatanana on 2022/08/15.
+//
+
+import SwiftUI
+
+@main
+struct KaraKanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
